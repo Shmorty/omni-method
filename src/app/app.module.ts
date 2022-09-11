55a516dd9a1ff1c5577e0f4ed1f9b73c6b6cc0c1
Paul Martel <shmorty@gmail.com>
2022-09-11 11:18:54 -0400

    push from laptop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { environment } from 'src/environments/environment';
 // import { OAuthModule } from 'angular-oauth2-oidc'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+// import { storeFreeze } from 'ngrx-store-freeze';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { assessmentReducer } from './store/reducers/assessment.reducer';
 import { FormsModule } from '@angular/forms';
 import { userReducer } from './store/reducers/user.reducer';
+
+// export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
+// export const storeDevTools: ModuleWithProviders[] =
+//     !environment.production ? [StoreDevtoolsModule.instrument()] : [];
 
 
 @NgModule({
