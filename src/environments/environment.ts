c6b3dfb8d2c1a0ab32838141c70a8dc4240321f7
Paul Martel <shmorty@gmail.com>
2022-11-24 12:22:34 -0500

    add configuration for uat/pre-prod backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,10 @@ export const environment = {
   API_KEY: '',
   // API_URL : 'https://api.shmorty.com/omnimethod/v1/'
   API_URL: 'https://jsonplaceholder.typicode.com/todos/1',
-  baseUrl: 'https://7crsalgmhk.execute-api.us-east-1.amazonaws.com',
+  // dev
+  // baseUrl: 'https://7crsalgmhk.execute-api.us-east-1.amazonaws.com',
+  // uat/pre-prod
+  baseUrl: 'https://4aupny9zf2.execute-api.us-east-1.amazonaws.com',
 
   gapi_client_id:
     '677058047401-jf9aa0tuvobl14v8u07lvfa5ef7a3tct.apps.googleusercontent.com',
