5ba76d331684dd96c3c86e20dbb5687d17dd4ee9
Paul Martel <shmorty@gmail.com>
2023-01-28 18:44:05 -0500

    setup ngrx

--- src/app/store/actions/category.actions.ts
+++ src/app/store/actions/category.actions.ts
@@ -1,14 +1,35 @@
-import { Action } from "@ngrx/store";
-import { Category } from "../models/category.model";
+import { Action } from '@ngrx/store';
+import { Category } from '../models/category.model';
 
 export enum CategoryActionType {
-    GET_CATEGORIES = '[Category] Get Categories',
+  GET_CATEGORIES = '[Category] Get Categories',
+  LOAD_CATEGORIES_BEGIN = '[Category] Load Categories Begin',
+  LOAD_CATEGORIES_SUCCESS = '[Category] Load Categories Success',
+  LOAD_CATEGORIES_FAILURE = '[Category] Load Categories Failure',
 }
 
 export class GetCategoriesAction implements Action {
-    readonly type = CategoryActionType.GET_CATEGORIES;
+  readonly type = CategoryActionType.GET_CATEGORIES;
 
-    constructor () {}
+  constructor() {}
 }
 
-export type CategoryAction = GetCategoriesAction;
+export class LoadCategoriesBegin implements Action {
+  readonly type = CategoryActionType.LOAD_CATEGORIES_BEGIN;
+}
+
+export class LoadCategoriesSuccess implements Action {
+  readonly type = CategoryActionType.LOAD_CATEGORIES_SUCCESS;
+  constructor(public payload: { data: any }) {}
+}
+
+export class LoadCategoriesFailure implements Action {
+  readonly type = CategoryActionType.LOAD_CATEGORIES_FAILURE;
+  constructor(public payload: { error: any }) {}
+}
+
+export type CategoryAction =
+  | GetCategoriesAction
+  | LoadCategoriesBegin
+  | LoadCategoriesSuccess
+  | LoadCategoriesFailure;
--- src/app/store/reducers/category.reducer.ts
+++ src/app/store/reducers/category.reducer.ts
@@ -6,10 +6,14 @@ import {
 
 export interface CategoryState {
   categories: Category[];
+  loading: boolean;
+  error: any;
 }
 
 export const initialState: CategoryState = {
   categories: [],
+  loading: false,
+  error: null,
 };
 
 export function reducer(
@@ -20,6 +24,29 @@ export function reducer(
     case CategoryActionType.GET_CATEGORIES:
       return {
         categories: [...state.categories],
+        loading: false,
+        error: null,
+      };
+      break;
+    case CategoryActionType.LOAD_CATEGORIES_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+      break;
+    case CategoryActionType.LOAD_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        categories: action.payload.data,
+      };
+      break;
+    case CategoryActionType.LOAD_CATEGORIES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
       };
       break;
     default:
