cf82ac6f94022c52af0336da0640bd9facd317cb
Paul Martel <shmorty@gmail.com>
2023-07-16 14:04:54 -0400

    onboarding progress

--- src/app/pages/onboarding/onboarding.page.ts
+++ src/app/pages/onboarding/onboarding.page.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {selectAllAssessments} from '../../store/assessments/assessment.selector';
 import {Store} from '@ngrx/store';
+import {Assessment} from 'src/app/store/assessments/assessment.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-onboarding',
@@ -8,22 +10,47 @@ import {Store} from '@ngrx/store';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
-
+  isInfoOpen = false;
   public assessments$ = this.store.select(selectAllAssessments);
   public step = 1;
+  public assessmentCount = 0;
+  public curAssessment: Assessment;
+  public values = [];
 
   constructor(
-    private store: Store
+    private store: Store,
+    private router: Router
   ) {}
 
   ngOnInit() {
+    this.assessments$.subscribe((arr) => this.assessmentCount = arr.length);
   }
 
   previous() {
-    console.log("previous", --this.step);
+    --this.step;
   }
 
   next() {
-    console.log("next", ++this.step);
+    if ((this.step + 1) < this.assessmentCount) {
+      ++this.step;
+    } else {
+      this.router.navigate(['home']);
+    }
   }
+
+  save(assessment: Assessment) {
+    console.log("save", assessment);
+    console.log("value", this.values[assessment.aid]);
+    this.next();
+  }
+
+  openInfo(assessment: Assessment) {
+    this.curAssessment = assessment;
+    this.setOpen(true);
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isInfoOpen = isOpen;
+  }
+
 }
