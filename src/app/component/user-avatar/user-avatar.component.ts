1725803c02bf2e76ec1df5b2157671b5137bf6a8
Paul Martel <shmorty@gmail.com>
2023-09-12 22:48:59 -0400

    avitar updates

--- src/app/component/user-avatar/user-avatar.component.ts
+++ src/app/component/user-avatar/user-avatar.component.ts
@@ -15,23 +15,26 @@ import {User} from 'src/app/store/user/user.model';
       <ion-label *ngIf="!user.avatar" class="textAvatar" color="color">
         {{user.firstName | slice : 0 : 1 }}{{user.lastName | slice : 0 : 1 }}
       </ion-label>
-      <ion-icon name="pencil-outline"></ion-icon>
+      <ion-icon id="icon" name="pencil-outline"></ion-icon>
+      <ion-label id="label" *ngIf="label">{{label}}</ion-label>
     </ion-avatar>
   `,
   styles: [
     `
       #avatar {
+        border-radius: 50%;
+        display: block;
         margin: auto;
         padding-left: 0px;
-        border-radius: 50%;
         /* aspect-ratio: 1 / 1; */
         /* overflow: hidden; */
+        position:relative;
       }
-      ion-icon {
+      #icon {
           color: var(--ion-color-primary);
-          position: relative;
-          bottom: 24px;
-          right: -40px;
+          position: absolute;
+          top: 28px;
+          right: -15px;
           background: white;
           border-radius: 50%;
           padding: 4px;
@@ -45,15 +48,22 @@ import {User} from 'src/app/store/user/user.model';
         width: 3em;
         height: 3em;
       }
+      #label {
+        position: absolute;
+        bottom: -8px;
+        left: 6px;
+      }
     `,
   ],
 })
 export class UserAvatarComponent implements OnInit {
   @Input() user: User;
+  @Input() label: string | undefined;
   constructor() {}
 
   ngOnInit() {
     console.log('UserAvatarComponent ngOnInit');
-    console.log(this.user);
+    console.log("user", this.user);
+    console.log("label", this.label);
   }
 }
