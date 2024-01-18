b8a99c6aa0a8b7303f99374d915118a1e5bbb1d3
Paul Martel <shmorty@gmail.com>
2024-01-17 19:17:37 -0500

    number picker cleanup

--- src/app/component/number-picker/number-picker.component.ts
+++ src/app/component/number-picker/number-picker.component.ts
@@ -36,11 +36,9 @@ export class NumberPickerComponent implements OnInit, OnChanges {
       console.log("reverse");
       this.range = this.range.reverse();
     }
-    // console.log("ngOnInit value", this.value);
-    // console.log("ngOnInit range", this.range);
+    // find index of initial value
     this.startIndex = this.range.indexOf(this.value);
     console.log("ngOnInit startIndex", this.startIndex);
-    // this.viewPort.scrollToIndex(this.startIndex);
   }
 
   ngAfterViewInit(): void {
@@ -48,14 +46,10 @@ export class NumberPickerComponent implements OnInit, OnChanges {
 
     // set initial value after delay
     setTimeout(() => {
-      var currentRange = this.viewPort.getRenderedRange();
-      console.log("currentRange before", currentRange);
-      this.viewPort.checkViewportSize();
+      // scroll to initial value
       console.log("scrollTo startIndex", this.startIndex);
       this.viewPort.scrollToIndex(this.startIndex);
-      currentRange = this.viewPort.getRenderedRange();
-      console.log("currentRange after", currentRange);
-    }, 200);
+    }, 50);
 
     // subscribe to updates
     this.viewPort.scrolledIndexChange.subscribe((index) => {
@@ -65,10 +59,9 @@ export class NumberPickerComponent implements OnInit, OnChanges {
         clearTimeout(this.timeoutId);
         Haptics.selectionStart();
       }
-      // console.log("indexChange scrollToIndex", index);
+
       if (this.curIndex !== index) {
         this.curIndex = index;
-        // console.log("viewPort.scrolledIndexChange emit", this.range[index]);
         this.valueChange.emit(this.range[index]);
       }
       // center selection
@@ -99,11 +92,6 @@ export class NumberPickerComponent implements OnInit, OnChanges {
         {length: len},
         (_, index) => (this.min * reciprocal + index / reciprocal * reciprocal) / reciprocal);
     }
-    // return Array.from(
-    //   {length: len},
-    //   (_, index) => this.min + index * this.increment);
-    // (_, index) => (this.min + index) / reciprocal);
-    //(this.min + index * multiplier * this.increment) / multiplier
   }
 
   ngOnChanges(changes: SimpleChanges): void {
